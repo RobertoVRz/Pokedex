708fbaa413f5f65eca3a7a6c1d0774c3410cf3b7
RobertoVRz <roberto.vzrz@gmail.com>
2023-06-28 14:29:48 -0600

    checkpoint

--- src/components/cards/CardPokemon.jsx
+++ src/components/cards/CardPokemon.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Image from "next/image";
 
 function CardPokemon() {
-  return <div>CardPokemon</div>;
+  return (
+    <div className="card w-full bg-base-100 shadow-xl">
+      <figure className="px-10 mt-10">
+        <Image
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Shoes"
+          className="object-cover"
+          width={100}
+          height={100}
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">Shoes!</h2>
+
+        <div className="card-actions">
+          <button className="btn btn-primary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CardPokemon;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,13 @@
 import { Inter } from "next/font/google";
+import CardPokemon from "@/components/cards/CardPokemon";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div
-      className="hero min-h-screen bg-white"
-      style={{
-        backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")`,
-      }}
-    >
+    <div className="hero min-h-screen bg-white">
       <div className="hero-content text-center text-neutral-content">
-        <div className="bg-zinc-300 w-96 h-48 flex flex-row justify-around items-center">
-          <div className="bg-black w-20 h-20"></div>
-          <div>
-            <h2>Nombre</h2>
-            <h2>ID</h2>
-            <h2>Tipo</h2>
-          </div>
-        </div>
+        <CardPokemon />
       </div>
     </div>
   );
